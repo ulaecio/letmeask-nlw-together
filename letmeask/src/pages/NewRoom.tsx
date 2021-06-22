2b722eb4131c7773f5ff991724f97e76486eca48
ulaecio <ulaecio@live.com>
2021-06-22 20:33:13 -0300

    Create Component Button, NewRoom page and Styles

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -5,7 +5,7 @@ import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 
-export function Home() {
+export function NewRoom() {
     return(
         <div id="page-auth">
             <aside>
@@ -16,20 +16,19 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask"/>
-                    <button>
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>
                     <form>
                         <input
                         type="text"
-                        placeholder="Digite o codigo da sala"
+                        placeholder="Nome da sala"
                         />
                         <Button type="submit">
-                            Entrar na sala
+                            Criar sala
                         </Button>
                     </form>
+                    <p>
+                        Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+                    </p>
                 </div>
             </main>
         </div>
