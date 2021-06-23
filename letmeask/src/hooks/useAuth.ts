4a5229317ecd216abaab093281bc58213e8cd5b5
ulaecio <ulaecio@live.com>
2021-06-23 01:10:04 -0300

    Create contexts

---
+++ letmeask/src/hooks/useAuth.ts
@@ -0,0 +1,8 @@
+import { useContext, useAuth } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+
+export function useAuth() {
+    const value = useContext(AuthContext)
+
+    return value;
+}
--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,18 +1,17 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 import '../styles/auth.scss';
 
 
-import { AuthContext } from '../App'
 
 
 export function Home() {
 const history = useHistory();
-const {user, signInWithGoogle} = useContext(AuthContext)
+const {user, signInWithGoogle} = useAuth()
 
 async function handleCreateRoom() {
     if (!user) {
