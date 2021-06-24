a01e88ad53de246457e825e2789f0a23c2269a2c
ulaecio <ulaecio@live.com>
2021-06-23 23:16:11 -0300

    Add and style room Page

---
+++ letmeask/src/components/RoomCode.tsx
@@ -0,0 +1,14 @@
+import copyImg from '../assets/images/copy.svg'
+
+import '../styles/room-code.scss';
+
+export function RoomCode () {
+    return (
+        <button className="room-code">
+            <div>
+                <img src={copyImg} alt="Copy room code"/>
+            </div>
+            <span>Sala #-Mcv6vdxZIbcSom73NoX</span>
+        </button>
+    )
+}
